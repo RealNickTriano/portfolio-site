69a3fe26d8908ca5de84e4a7791f12fc69a1f6ab
RealNickTriano <70707042+RealNickTriano@users.noreply.github.com>
2022-07-06 16:19:47 -0400

    qol animation with navbar

--- index.js
+++ index.js
@@ -2,12 +2,31 @@ const home = document.querySelector('#home');
 const projects = document.querySelector('#projects');
 const about = document.querySelector('#about');
 const contact = document.querySelector('#contact');
+const copyright = document.querySelector('#copyright');
 
 const projectSection = document.querySelector('.projects');
 const aboutSection = document.querySelector('.about-me');
 const contactSection = document.querySelector('.contact-me');
+const mainHeader = document.querySelector('.main-header');
 
 const nav = document.getElementById('main-nav');
+
+const year = new Date().getFullYear();
+
+copyright.innerHTML = `&copy; ${year} Nicholas Triano`;
+
+//Handle navabar change on scroll
+window.onscroll = () => {
+    if(document.documentElement.scrollTop > 50)
+    {
+        nav.classList.add('navbar-scroll');
+        mainHeader.classList.add('mt-100');
+    }
+    else{
+        nav.classList.remove('navbar-scroll');
+        mainHeader.classList.remove('mt-100');
+    }
+}
 
 home.addEventListener('click', (event) => {
     event.preventDefault();
